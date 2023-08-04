56259faed8b13a22749a07db8e37ca390a670342
agasefamircan <agasefemircanov@gmail.com>
2023-08-05 02:30:07 +0400

    added methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  length:number= 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  modifyLetter() {
+    this.includeLetters = !this.includeLetters;
+  }
+  modifySymbols() {
+    this.includeLetters = !this.includeSymbols;
+  }
+  modifyNumbers() {
+    this.includeLetters = !this.includeNumbers;
+  }
+  btnclick() {
+    console.log(this.includeLetters);
+  }
+  modifyLength(value: string) {
+    const numb = parseInt(value);
+    if (!isNaN(numb)) {
+      debugger;
+      this.length = numb;
+    }
+  }
 }
