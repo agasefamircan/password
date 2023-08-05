d48df5b0a91ca73fd5307e536ea134aac9e7481b
agasefamircan <agasefemircanov@gmail.com>
2023-08-05 16:46:11 +0400

    password generated functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  length:number= 0;
+  password = '';
+  length: number = 0;
   includeLetters: boolean = false;
   includeNumbers: boolean = false;
   includeSymbols: boolean = false;
@@ -15,18 +16,42 @@ export class AppComponent {
     this.includeLetters = !this.includeLetters;
   }
   modifySymbols() {
-    this.includeLetters = !this.includeSymbols;
+    this.includeSymbols = !this.includeSymbols;
   }
   modifyNumbers() {
-    this.includeLetters = !this.includeNumbers;
+    this.includeNumbers = !this.includeNumbers;
   }
   btnclick() {
-    console.log(this.includeLetters);
+    const numbers = '0123456789';
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let validChars = '';
+
+    if (!(this.includeLetters || this.includeNumbers || this.includeSymbols)) {
+      alert("Please check at least one box");
+    }
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+     if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
   modifyLength(value: string) {
     const numb = parseInt(value);
     if (!isNaN(numb)) {
-      debugger;
       this.length = numb;
     }
   }
